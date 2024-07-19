5b247b5d8b080db59682d2a68e1b57a2a7ecfe86
Christopherfrommaine <christopherfrommaine@gmail.com>
2024-07-19 18:34:54 -0400

    ASTfromRP works!

--- wff.py
+++ wff.py
@@ -1,151 +1,135 @@
-operatorTokens = '=¬∧∨∀∃⇔⇒∅∈&^|'
+operatorTokens = '⚠¬!∧&∨|⊻^∀∃⇒⇔='  # In order of precedence, must keep the error symbol at the beginning, as it is the symbol for variables, which have precedence over everything else.
+whitespaceTokens = ' \n'
+breakOnChars = operatorTokens + whitespaceTokens + '()[]{}'
+operatorPrecedence = {opSymbol: i for i, opSymbol in enumerate(operatorTokens)} 
+
 print('here are some helpful charachters for building logical statements: \n ' + operatorTokens)
-
-
-def removeAll(el, l):
-    for _ in range(l.count(el)):
-        l.remove(el)
-
 
 class op:
     def __init__(self, val1, val2):
         self.v1 = val1
         self.v2 = val2
-        self.opSymbol = None
 
-    aryness = 2
-
-    def wff(self):
-        return f'({self.v1.wff()}){self.opSymbol}({self.v2.wff()})'
-
-    def eqWFF(self, other):
-        return self.wff() == other.wff()
-
-    def eqNum(self, other):
-        pass
+    arity = 2
+    symbol = '⚠'
 
     def __repr__(self):
-        return f'({self.v1}){self.opSymbol}({self.v2})'
-        # return f'{type(self)}({self.v1}, {self.v2})'
-
+        inParens = lambda v: f'({v})' if operatorPrecedence[type(self).symbol] < operatorPrecedence[type(v).symbol] else f'{v}'
+        match type(self).arity:
+            case 1:
+                return type(self).symbol + inParens(self.v1)
+            case 2:
+                return inParens(self.v1) + type(self).symbol + inParens(self.v2)
+            case _:
+                raise Exception('wrong arity')
+   
     def __invert__(self):
         return opNot(self)
-
     def __or__(self, other):
         return opOr(self, other)
-
     def __and__(self, other):
         return opAnd(self, other)
-
     def __ror__(self, other):
         return self.__or__(other)
-
     def __rand__(self, other):
         return self.__and__(other)
 class opNot(op):
     def __init__(self, val1):
         op.__init__(self, val1, None)
-        self.opSymbol = '¬'
     
-    aryness = 1
+    symbol = '¬'
+    arity = 1
 
-    def __repr__(self):
-        return f'{self.opSymbol}({self.v1})'
 class opOr(op):
-    def __init__(self, val1, val2):
-        op.__init__(self, val1, val2)
-        self.opSymbol = '∨'
+    symbol = '∨'
 class opAnd(op):
-    def __init__(self, val1, val2):
-        op.__init__(self, val1, val2)
-        self.opSymbol = '∧'
+    symbol = '∧'
+class opXor(op):
+    symbol = '⊻'
+class opFA(op):
+    symbol = '∀'
+class opTE(op):
+    symbol = '∃'
 class opIf(op):
-    def __init__(self, val1, val2):
-        op.__init__(self, val1, val2)
-        self.opSymbol = '⇒'
+    symbol = '⇒'
 class opImp(op):
-    def __init__(self, val1, val2):
-        op.__init__(self, val1, val2)
-        self.opSymbol = '⇔'
+    symbol = '⇔'
 class opVar(op):
     def __init__(self, name, value=None):
         op.__init__(self, None, None)
         self.name = name
         self.value = value
-    
-    aryness = 1
+
+        if self.name == 'True':
+            self.value = True
+        if self.name == 'False':
+            self.value = False
+    arity = None
+    symbol = '⚠'
 
     def __repr__(self):
         return self.name
 
-def tokenizer(string):
+opFromTok = {'¬': opNot, '!': opNot, '∧': opAnd, '&': opAnd, '∨': opOr, '|': opOr, '⊻': opXor, '^': opXor, '∀': opFA, '∃': opTE, '⇒': opIf, '⇔': opImp, '=': opImp} 
+
+
+def tokenize(string):
     o = []
     temp = ''
-    parensDepth = 0
     for char in string:
-        if not parensDepth and char in operatorTokens:
-            o.append(temp)
-            o.append(char)
-            temp = ''
+        if char in breakOnChars:
+            if temp != '':
+                o.append(temp)
+                temp = ''
+            if char not in whitespaceTokens:
+                o.append(char)
         else:
             temp += char
-
-        if char == '(':
-            temp = ''
-            parensDepth += 1
-        if char == ')':
-            parensDepth -= 1
-            if not parensDepth:
-                o.append(tokenizer(temp[:-1]))
-                temp = ''
-    o.append(temp)
-    removeAll('', o)
+    if temp:
+        o.append(temp)
     return o
 
-def parser(tokens):
-    # Parens
-    tokens = [parser(tok) if isinstance(tok, list) else tok for tok in tokens]
-    # Not
-    index = 0
-    cont = True
-    while cont:
-        cont = False
-        for i, tok in enumerate(tokens):
-            if tok == '¬':
-                tokens = tokens 
-
-    return tokens
-
-
 def shuntingYard(tokens):
-    precedence {'¬': 1, '∧': 2, '∨': 3, '∀': 4, '∃': 5, '⇒': 6, '⇔': 7}
-    
-
-
-def evalReversePolish(rp):
-    operationFromTok = {'¬': opNot, '∧': opAnd, '∨': opOr, '⇒': opIf, '⇔': opImp}
-    
     stack = []
-    for tok in rp:
+    queue = []
+    for tok in tokens:
         if tok in operatorTokens:
-            oper = operationFromTok(tok)
-            ary = oper.aryness
-            stack = stack[:-ary] + [operationFromTok(tok)(stack[-ary:])
-        else:
+            while stack and stack[-1] in operatorTokens and operatorPrecedence[stack[-1]] < operatorPrecedence[tok]:
+                queue.append(stack.pop())
             stack.append(tok)
-    return stack[-1]
+        elif tok == '(':
+            stack.append('(')
+        elif tok == ')':
+            while stack[-1] != '(':
+                queue.append(stack.pop())
+            stack.pop()
+        else:
+            queue.append(tok)
+    queue += reversed(stack)
+    return queue
+
+
+def ASTfromRP(tokens):
+    solveStack = []
+    for tok in tokens:
+        if tok in operatorTokens:
+            opType = opFromTok[tok]
+            solveStack = solveStack[:-opType.arity] + [opType(*solveStack[-opType.arity:])]
+        else:
+            solveStack.append(opVar(tok))
+    if len(solveStack) != 1:
+        raise Exception("Too few operators")
+    return solveStack[0]
 
 
 test_L = '¬p∧¬q⇔¬(p∨q)'
+test_tokens = tokenize(test_L)
+print(test_tokens)
 
-T = tokenizer(test_L)
-print(T)
-print(parser(T))
+test_L = ' ¬p  ∧\n¬ q        ⇔¬ (                          p \n\n\n\n∨q)'
+test_tokens = tokenize(test_L)
+print(test_tokens)
 
+print(shuntingYard(test_tokens))
+print(ASTfromRP(shuntingYard(test_tokens)))
 
-class statement:
-    def __init__(self, stringRepr='T'):
-        self.str = stringRepr
-
-        self.variables = []
-        self.ast = None
